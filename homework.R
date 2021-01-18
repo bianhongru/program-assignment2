e5080500f1eb1f0e3b326a5e47c9126187e86aa0
bianhongru <75818241+bianhongru@users.noreply.github.com>
2021-01-18 21:48:54 +0800

    Update homework.R

--- homework.R
+++ homework.R
@@ -1,1 +1,22 @@
-
+makeCacheMatrix <- function(x = matrix()) {
+   ig <- NULL
+   set <- function(y) {
+      x <<- y
+      ig <<- NULL
+   }
+   get <- function() x
+   setinverse <- function(inverse) ig <<- inverse
+   getinverse <- function() ig
+   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+cacheSolve <- function(x, ...) {
+   ig <- x$getinverse()
+   if(!is.null(ig)) {
+      message("getting cached data.")
+      return(ig)
+   }
+   data <- x$get()
+   ig <- solve(data)
+   x$setinverse(ig)
+   ig
+}
